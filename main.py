2ba3b1ee055cf5dd4e5b8985d309b17b8477aac1
guisena08 <guisena@ufc.alu.br>
2023-04-21 10:21:21 -0300

    tarefa concluida, mandei o arquivo errado por engano

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-
+numero = int(input("digite um numero "))
+if numero % 3==0 and numero % 5==0:
+                print("fizzBuzz") 
+                
+elif numero % 3==0:
+            print("fizz")
+elif numero % 5==0:
+            print('buzz')
+else:
+        print(numero)
  
          
           
